29d9d2aba42a43d25d0ff8ccc649a41b735dcdb5
Otabek33 <kebatotabek33@gmail.com>
2023-12-19 16:04:35 +0500

    config done location view

--- wePrint/settings.py
+++ wePrint/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "leaflet",
     "debug_toolbar",
     "apps.accounts",
     "apps.clients",
