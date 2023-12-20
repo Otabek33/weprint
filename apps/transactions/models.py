d782e362432349bba5a26152ebe3be581ea59608
Otabek33 <kebatotabek33@gmail.com>
2023-12-20 17:59:05 +0500

    transaction model is created

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,3 +1,70 @@
+from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from apps.accounts.models import CustomUser
+from apps.clients.models import Client
+from apps.products.models import Product
+
 
 # Create your models here.
+
+class CashType(models.IntegerChoices):
+    CASH = 1, _('Naqt')
+    BANK = 2, _('Bank')
+    PRODUCT = 3, _('Mahsulot')
+
+
+class DoubleEntryAccounting(models.IntegerChoices):
+    CREDIT = 1, _('Kredit')
+    DEBIT = 2, _('Debit')
+
+
+class Transaction(models.Model):
+    """Class representing a person"""
+
+    description = models.CharField("Ta'rif", blank=True, max_length=55)
+    cash_type = models.IntegerField("Pul turi",
+                                    choices=CashType.choices,
+                                    default=CashType.CASH)
+    client = models.ForeignKey(Client,
+                               on_delete=models.SET_NULL,
+                               blank=True,
+                               null=True,
+                               related_name="client_transaction", )
+    product = models.ForeignKey(Product,
+                                on_delete=models.SET_NULL,
+                                blank=True,
+                                null=True,
+                                related_name="product_transaction", )
+    double_entry_accounting = models.IntegerField("Pul yo'nalishi",
+                                                  choices=DoubleEntryAccounting.choices,
+                                                  default=DoubleEntryAccounting.CREDIT)
+    balance = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    created_by = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="transaction_created_by",
+    )
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_by = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="transaction_updated_by",
+    )
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_status = models.BooleanField(default=False)
+
+    class Meta:
+        """Class representing a person"""
+        verbose_name = "Транзакция"
+        verbose_name_plural = "Транзакция"
+
+    def __str__(self) -> str:
+        return str(self.description)
--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from typing import Any, Dict
+from datetime import datetime, timezone
 
-# Create your views here.
+from django.forms.models import BaseModelForm
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
+from django.views.generic import (ListView, UpdateView, DeleteView)
+from apps.products.models import Product as ProductModel
+from apps.products.forms import ProductCreateForm, ProductUpdateForm
+from apps.transactions.models import Transaction
+from utils.helpers import is_ajax
+
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = "transactions/transaction_list.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['transaction_list'] = Transaction.objects.filter(created_by=self.request.user, deleted_status=False)
+        return context
+
+
+product_list = TransactionListView.as_view()
