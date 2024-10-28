5abc50f26353148da1f193d672fcd3c61643f005
Otabek33 <kebatotabek33@gmail.com>
2024-10-28 14:02:06 +0500

    update productiin setting

--- wePrint/settings/production.py
+++ wePrint/settings/production.py
@@ -8,7 +8,7 @@ DEBUG = env.bool("DJANGO_DEBUG")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["print-book.uz"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["print.fintechdev.uz"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
